6120f5170fb08ef83342f9613c3e9a3b0fc53b03
Bmeza05 <brianmeza952@gmail.com>
2021-07-07 19:08:55 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const onmouseover = () => console.log(mouseOverFunction(this))
+
+const getElementById = () => {
+    setBackgroundColorById("paragraph", "blue");
+}
+
+const getElementById = () =>{
+    alert(document.getElementById("popup-input").value);
+}
+
+const getElementById = () =>{
+    setBackgroundColorById("body", "red");
+} 
+
+const getElementById = () =>{
+    setBackgroundColorById("body", "white");
+}
+
+const getValueFromId = () =>{
+    return document.getElementById(id).value;
+}
+
+
+const setBackgroundColorById = () =>{
+    document.getElementById(id).style = "background-color: " + color;
+}
+
+const mouseOverFunction = () =>{
+    el.style = "background-color: black";
+}
